655a39d957e1dcfdff5830d912ddf60df096a13f
marciojosebaldo <marciojosebaldo@gmail.com>
2023-03-07 19:52:51 -0300

    Add files via upload

---
+++ entradaNumeroBinario.java
@@ -0,0 +1,16 @@
+/*Exemplo com entrada em número binário e impressão*/
+
+import java.util.Scanner;
+
+public class entradaNumeroBinario {
+	public static void main (String[] args) {
+		
+		int binario = 0b100;
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println(binario);
+	
+		entrada.close();
+	}
+}
