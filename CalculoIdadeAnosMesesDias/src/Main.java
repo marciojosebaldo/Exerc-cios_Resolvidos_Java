3fd27ba5388d1d5ac723bb0f2dc0b7b4527a13a1
marciojosebaldo <marciojosebaldo@gmail.com>
2023-05-15 20:51:44 -0300

    Add files via upload

--- CalculoIdadeAnosMesesDias/src/Main.java
+++ CalculoIdadeAnosMesesDias/src/Main.java
@@ -1,28 +1,26 @@
 import java.time.*;
 import java.util.Scanner;
 
-public class Main {
+        public class Main {
 
-    public static void main(String[] args) {
+            public static void main(String[] args) {
 
-        LocalDate hoje = LocalDate.now();
+                LocalDate hoje = LocalDate.now();
 
-        Scanner entrada = new Scanner(System.in);
+                Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Informe a data de nascimento no formato dd/MM/yyyy: ");
-        String dataNascimentoString = entrada.next();
+                System.out.println("Informe a data de nascimento no formato dd/MM/yyyy: ");
+                String dataNascimentoString = entrada.next();
 
-        LocalDate dataNascimentoPersonalizada = LocalDate.parse(dataNascimentoString,
-                java.time.format.DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                LocalDate dataNascimentoPersonalizada = LocalDate.parse(dataNascimentoString,
+                        java.time.format.DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 
-        Period idadeAno = Period.between(dataNascimentoPersonalizada, hoje);
-        int idadeMes = idadeAno.getMonths() + (idadeAno.getYears() * 12);
-        double idadeDias = (double) idadeAno.getDays() + (idadeAno.getYears() * 365.25);
+                Period idadeAno = Period.between(dataNascimentoPersonalizada, hoje);
+                int idadeMes = idadeAno.getMonths() + (idadeAno.getYears() * 12);
+                double idadeDias = (double) idadeAno.getDays() + (idadeAno.getMonths() * 30) + (idadeAno.getYears() * 365.25);
 
-        System.out.println("Sua idade em anos é: " + idadeAno.getYears());
-        System.out.println("Sua idade em meses é: " + idadeMes);
-        System.out.println("Sua idade em dias é: " + idadeDias);
-
-
+                System.out.println("Sua idade em anos é: " + idadeAno.getYears());
+                System.out.println("Sua idade em meses é: " + idadeMes);
+                System.out.println("Sua idade em dias é: " + idadeDias);
     }
 }
